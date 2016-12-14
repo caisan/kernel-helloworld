6b55088f56992ba73b30127e529a852d60e50a5d
shan.cai <shan.cai@eayun.com>
2016-12-14 10:30:14 +0800

    new comment

--- hello.c
+++ hello.c
@@ -5,7 +5,7 @@
  * #include <stdlib.h>*/
 MODULE_LICENSE("BSD");
 MODULE_AUTHOR("caisan");
-
+/*next we will make this module to be a char device module*/
 static int hello_init(void)
 {
     printk(KERN_ALERT "hello wolrd");
@@ -15,7 +15,6 @@ static void hello_exit(void)
 {
     printk(KERN_ALERT "good byeworld");
 }
-/*this is a new hello */
 module_init(hello_init);
 module_exit(hello_exit);
 
